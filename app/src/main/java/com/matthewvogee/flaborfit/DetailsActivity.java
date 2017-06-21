696b5f44ce303f37e486b22b318f81ec7817b4c6
Matthew Vogee <mvogee@yahoo.com>
2017-06-21 14:46:50 -0700

    layout changes based on which page is visited

--- app/src/main/java/com/matthewvogee/flaborfit/DetailsActivity.java
+++ app/src/main/java/com/matthewvogee/flaborfit/DetailsActivity.java
@@ -1,8 +1,10 @@
 package com.matthewvogee.flaborfit;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -12,7 +14,24 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        TextView exerciseTitle = (TextView)findViewById(R.id.exerciseTitle);
+        TextView exerciseText = (TextView)findViewById(R.id.exerciseTitle);
         ImageView exerciseImage = (ImageView)findViewById(R.id.exerciseImage);
+        LinearLayout mainBG = (LinearLayout)findViewById(R.id.mainBG);
+
+        String exerciseTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
+        exerciseText.setText(exerciseTitle);
+
+        if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_WEIGHTS)) {
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.weight, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));
+        }
+        else if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_YOGA)) {
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.lotus, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#916bcd"));
+        }
+        else {
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.heart, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#52ad56"));
+        }
     }
 }
